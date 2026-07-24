b0108c7b6b8431b35294e9dd4feee618059db4ba
Linda Nguyen <l.nguyen92404@gmail.com>
2026-07-24 12:19:11 -0700

    make verify?

--- hack/validation/taint.sh
+++ hack/validation/taint.sh
@@ -12,6 +12,12 @@ yq eval '.spec.versions[0].schema.openAPIV3Schema.properties.spec.properties.sta
 yq eval '.spec.versions[0].schema.openAPIV3Schema.properties.spec.properties.startupTaints.items.properties.value.pattern = "^([a-z0-9]([-a-z0-9]*[a-z0-9])?(\.[a-z0-9]([-a-z0-9]*[a-z0-9])?)*(\/))?([A-Za-z0-9][-A-Za-z0-9_.]*)?[A-Za-z0-9]$"' -i pkg/apis/crds/karpenter.sh_nodeclaims.yaml
 yq eval '.spec.versions[0].schema.openAPIV3Schema.properties.spec.properties.startupTaints.items.properties.effect.enum += ["NoSchedule","PreferNoSchedule","NoExecute"]' -i pkg/apis/crds/karpenter.sh_nodeclaims.yaml
 
+## Cloud-Provider-Startup-Taint
+yq eval '.spec.versions[0].schema.openAPIV3Schema.properties.status.properties.cloudProviderStartupTaints.items.properties.key.minLength = 1' -i pkg/apis/crds/karpenter.sh_nodeclaims.yaml
+yq eval '.spec.versions[0].schema.openAPIV3Schema.properties.status.properties.cloudProviderStartupTaints.items.properties.key.pattern = "^([a-z0-9]([-a-z0-9]*[a-z0-9])?(\.[a-z0-9]([-a-z0-9]*[a-z0-9])?)*(\/))?([A-Za-z0-9][-A-Za-z0-9_.]*)?[A-Za-z0-9]$"' -i pkg/apis/crds/karpenter.sh_nodeclaims.yaml
+yq eval '.spec.versions[0].schema.openAPIV3Schema.properties.status.properties.cloudProviderStartupTaints.items.properties.value.pattern = "^([a-z0-9]([-a-z0-9]*[a-z0-9])?(\.[a-z0-9]([-a-z0-9]*[a-z0-9])?)*(\/))?([A-Za-z0-9][-A-Za-z0-9_.]*)?[A-Za-z0-9]$"' -i pkg/apis/crds/karpenter.sh_nodeclaims.yaml
+yq eval '.spec.versions[0].schema.openAPIV3Schema.properties.status.properties.cloudProviderStartupTaints.items.properties.effect.enum += ["NoSchedule","PreferNoSchedule","NoExecute"]' -i pkg/apis/crds/karpenter.sh_nodeclaims.yaml
+
 # Nodepool Validation:
 ## Taint
 yq eval '.spec.versions[0].schema.openAPIV3Schema.properties.spec.properties.template.properties.spec.properties.taints.items.properties.key.minLength = 1' -i pkg/apis/crds/karpenter.sh_nodepools.yaml
